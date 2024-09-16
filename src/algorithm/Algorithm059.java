51b1be5f3fb283d34ccfcfb3ad0b7a25c612cc4e
hwk9407 <hwk__@naver.com>
2024-09-17 04:36:33 +0900

    59번 덧칠하기 문제 해결

---
+++ src/algorithm/Algorithm059.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+public class Algorithm059 {
+    /**
+     * 덧칠하기
+     */
+    public static void main(String[] args) {
+        int n = 10; // 벽 총 길이
+        int m = 4; // 페인트 롤러 크기
+        int[] section = {1, 5, 6, 7, 8};
+        int answer = 0;
+
+        int paintRange = 0;
+
+        for (int i : section) {
+            if (paintRange != 0 && i < paintRange) continue;
+            else {
+                paintRange = i + m;
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
