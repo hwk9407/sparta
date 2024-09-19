8da5afbaa2052a86d0cc1201657529a2bb6c0ad8
hwk9407 <hwk__@naver.com>
2024-09-19 11:19:54 +0900

    62번 옹알이 (2) 문제 해결

---
+++ src/algorithm/Algorithm062.java
@@ -0,0 +1,52 @@
+package algorithm;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Algorithm062 {
+    /**
+     * 062. 옹알이 (2)
+     * "aya", "ye", "woo", "ma" 네 가지 발음과 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못함
+     * 연속해서 같은 발음 못함
+     * babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수
+     */
+    public static void main(String[] args) {
+        String[] babbling = {"yeayaye"};
+        int answer = 0;
+
+        HashSet<String> canTalk = new HashSet<>(Arrays.asList("aya", "ye", "woo", "ma"));
+
+
+        for (String word : babbling) {
+            String lastDeleteString = "";
+            StringBuilder sb = new StringBuilder(word);
+            System.out.println("word: " + word);
+            while (true) {
+                boolean checkFlag = false;
+
+                for (String s : canTalk) {
+                    if (sb.length() == 0) {
+                        break;
+                    }
+
+                    if (sb.indexOf(s) == 0 && !lastDeleteString.equals(s)) {
+                        lastDeleteString = s;
+                        int startIndex = sb.indexOf(s);
+                        sb.delete(startIndex, startIndex + s.length());
+                        checkFlag = true;
+                        if (sb.length() == 0) {
+                            answer++;
+                        }
+                    }
+                }
+
+                if (!checkFlag) break;
+            }
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
