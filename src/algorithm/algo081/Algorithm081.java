5f952b1fb47a09e9ef7acb460ed2567b3b253a0e
hwk9407 <hwk__@naver.com>
2024-10-10 09:56:28 +0900

    81번 N개의 최소공배수 문제 해결

---
+++ src/algorithm/algo081/Algorithm081.java
@@ -0,0 +1,38 @@
+package algorithm.algo081;
+class Solution {
+    public int solution(int[] arr) {
+        int gcd = arr[0];
+        for (int num : arr) {
+            int lcm = getLcm(gcd, num);
+            gcd = (gcd * num) / lcm;
+        }
+
+        return gcd;
+    }
+
+    private int getLcm(int a, int b) {
+        int bigNum = Math.max(a, b);
+        int smallNum = Math.min(a, b);
+
+        int rest = bigNum % smallNum;
+        if (rest == 0) {
+            return smallNum;
+        }
+
+        return getLcm(smallNum, rest);
+    }
+}
+public class Algorithm081 {
+    /**
+     * N개의 최소공배수
+     * 두 수의 최소공배수 : 두 수의 배수 중 공통이 되는 가장 작은 숫자
+     * N개의 최소공배수 : N개의 수들의 배수 중 공통이 되는 가장 작은 숫자
+     */
+
+    public static void main(String[] args) {
+        int[] arr = {2, 6, 8, 14};
+        Solution solution = new Solution();
+        System.out.println(solution.solution(arr));
+
+    }
+}
